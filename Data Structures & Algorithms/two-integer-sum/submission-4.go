ce75aa2908d1afd4f5a29fcdd07c96e0cb96170f
Andrey Kalikin <kinzo.x.beato@gmail.com>
2026-06-18 12:46:14 +0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.go
@@ -0,0 +1,8 @@
+func twoSum(nums []int, target int) []int {
+    hash := map[int]int{}
+	for i,n := range nums {
+		if j,ok:=hash[target-n]; ok {return []int{j,i}}
+		hash[n] = i
+	}
+	return []int{}
+}
